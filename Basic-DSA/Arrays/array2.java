aabbb7cd265ad7067fd7b70bcdea2dfbcba5e6d3
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-05 02:17:37 +0530

    Add

--- Basic-DSA/Arrays/array2.java
+++ Basic-DSA/Arrays/array2.java
@@ -55,9 +55,55 @@ public class array2 {
         }
         System.out.println("Max of sub array sum: " + ms);
     }
+
+    //trapping rainwater
+    public static int trappedWater(int height[]) {
+        int n = height.length;
+        //left max array 
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        //right max array
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        int trappedWater = 0;
+        //loop
+        for (int i = 0; i < n; i++) {
+            //logic for waterlevel 
+            int waterlevel = Math.min(leftMax[i], rightMax[i]);
+            //Formula for calculating water
+            trappedWater += waterlevel - height[i];
+        }
+        return trappedWater;
+    }
+
+    //Buy and sell stocks
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if(buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
-        int number[] = {1, -2, 6, -1, 3};
+        //int number[] = {1, -2, 6, -1, 3};
         //maxSubArray(number);
-        maxSubArrayPrefix(number);
+        //maxSubArrayPrefix(number);
+        // int height[]= {4, 2, 0, 6, 3, 2, 5};
+        // System.out.println(trappedWater(height));
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println(buyAndSellStocks(prices));
     }
 }
