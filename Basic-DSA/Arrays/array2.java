bc130273376191eed5fced26e95401188679f514
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-04 18:47:02 +0530

    Add

--- Basic-DSA/Arrays/array2.java
+++ Basic-DSA/Arrays/array2.java
@@ -1,4 +1,5 @@
 public class array2 {
+    //Max sub array sum 
     public static void maxSubArray(int number[]) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
@@ -19,8 +20,44 @@ public class array2 {
         }
         System.out.println("Max of sub array sum: " + maxSum);
     }
+
+    //max sub array sum alt method
+    public static void maxSubArrayPrefix(int number[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[number.length];
+        prefix[0] = number[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + number[i];
+        }
+        for (int i = 0; i < number.length; i++) {
+            int start = i;
+            for (int j = i + 1; j < number.length; j++) {
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1]; 
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max of sub array sum: " + maxSum);
+    }
+
+    public static void kadane(int number[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for (int i = 0; i < number.length; i++) {
+            cs += number[i];
+            if(cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("Max of sub array sum: " + ms);
+    }
     public static void main(String[] args) {
-         int number[] = {1, -2, 6, -1, 3};
-        maxSubArray(number);
+        int number[] = {1, -2, 6, -1, 3};
+        //maxSubArray(number);
+        maxSubArrayPrefix(number);
     }
 }
