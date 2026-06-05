676abcd3742b94c9e287fcbc79ed1947eb7cc5c7
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-05 19:00:35 +0530

    Add

---
+++ Basic-DSA/2dArray/matrices.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class matrices {
+    public static boolean search(int arr[][], int key) {
+         for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == key) {
+                    System.out.print("Found at cell (" + i + "," + j + ")");
+                    return true;
+                }  
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = new int[3][3];
+        int n = arr.length, m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        search(arr, 5);
+    }
+}
