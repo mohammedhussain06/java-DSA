26c546ebca5a74bf45f541fc8e21b9ba08e5fb8c
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-13 15:02:43 +0530

    Add

---
+++ Side-quest/merge.java
@@ -0,0 +1,51 @@
+public class merge {
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        int mid = si + (ei - si) / 2; // dividing mid
+        mergeSort(arr, si, mid); // left side
+        mergeSort(arr, mid + 1, ei);
+        mergeArray(arr, si, mid, ei); // right side
+    }
+
+    public static void mergeArray(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid) {    //left array leftover element
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei) {     //right array leftover element 
+            temp[k++] = arr[j++];
+        }
+        for (k = 0, i = si; k < temp.length; k++, i++) {     //Copy temp to array 
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
