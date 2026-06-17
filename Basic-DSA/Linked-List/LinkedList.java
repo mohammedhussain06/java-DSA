5ea22d5f0994086ec41f47b656217a47990eb655
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-17 21:03:32 +0530

    Add

---
+++ Basic-DSA/Linked-List/LinkedList.java
@@ -0,0 +1,44 @@
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next; 
+
+        public Node(int data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    //Add first 
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //Add last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public static void main(String args[]) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+    }
+}
