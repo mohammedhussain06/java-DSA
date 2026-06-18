a866bb3c842d2dc129d0cb7cc7df6b571ed9c66e
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-18 10:25:43 +0530

    Add

--- Basic-DSA/Linked-List/LinkedList.java
+++ Basic-DSA/Linked-List/LinkedList.java
@@ -54,6 +54,114 @@ public class LinkedList {
         temp.next = newNode;
     }
 
+    //remove first
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    //remove last
+    public int removeLast() {
+         if (size == 0) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    //iterative search
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    //Recursive search
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    //Reverse LL
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    //Delete nth from end 
+    public void deleteNthFromEnd(int n) {
+        int sz = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+        if (n == sz) {
+            head = head.next;
+            return;
+        }
+        int i = 0;
+        Node prev = head;
+        while (i < sz - n) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
     // print
     public void print() {
         if (head == null) {
@@ -71,14 +179,24 @@ public class LinkedList {
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
         ll.Add(9, 2);
         ll.print();
         System.out.println(ll.size);
+        // ll.removeFirst();
+        // ll.print();
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(ll.size);
+        // System.out.println(ll.itrSearch(3));
+        // System.out.println(ll.itrSearch(10));
+        // System.out.println(ll.recSearch(3));
+        // System.out.println(ll.recSearch(10));
+        ll.reverse();
+        ll.print();
+        ll.deleteNthFromEnd(3);
+        ll.print();
     }
 }
