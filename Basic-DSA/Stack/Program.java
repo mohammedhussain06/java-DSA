940a1813c37fa32e50ba86a4d07c6fdc860b0b28
mohammedhussain06 <hussain2006.shaikh@gmail.com>
2026-06-19 12:41:12 +0530

    Add

---
+++ Basic-DSA/Stack/Program.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+public class Program {
+    //push at bottom
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);        
+    }
+
+    //reverse string 
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+
+    //reverse stack
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    //print
+    public static void printStack(Stack<Integer> s) {
+        while (s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reverseStack(s);
+        printStack(s);
+        // pushAtBottom(s, 4);
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.pop());
+        // }
+        // String str = "abc";
+        // System.out.println(reverseString(str));
+    }
+}
